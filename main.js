75bb210783a1808cb5ed61d79425cd452b4f73fa
pear23154 <88102558+pear23154@users.noreply.github.com>
2021-08-04 15:09:55 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,17 @@
 
-$(document).ready(function(){
-    update_amount();
-    $('.quantity,.price').on('keyup keypress blur change',function(e){
-        update_amount()
-    } )
+let trush=  document.getElementsByClassName('fas fa-trash');
+trush.map((el)=>
+el.addeventlistener('click',function(){
+    el.parentNode.remove()
 })
-function update_amount(){
-    var sum=0.0
-    $('#mytable > tbody > tr').each (function(){
-        var quantity= $(this).find('.quantity').val();
-        var price= $(this).find('.price').val();
-        var amount=(quantity*price)
-        sum+=amount;
-        $(this).find('.amount').text(''+amount);
-    })
-    $('.total') .text(sum);
-}
-var incrementQty;
-var decrementQty;
-var plusbutn = $('.cart-quantity-plus');
-var minusbtn= $('.cart-quantity-minus');
-var incrementQty=btnplus.click (function(){
-    var $n= $(this)
-    .parent('.button-container')
-    .find('.quantity');
-    $n.val(Number($n.val())+1);
-    update_amount();
-})
- var decrementQty=minusbtn.click(function(){
-     var $n=$(this)
-     .parent('.button-container')
-     .find ('.quantity')
-     var Qtyval= Number($n.val());
-     if(Qtyval>0){
-         $n.val(Qtyval-1)
-     }
-     update_amount()
- })
- console.log()
+)
+
+
+
+   
+
+
+   
+
 
   
